27692073ed580bd5767846b1e81c130e32bdcfc8
duysen <86411226+duysen@users.noreply.github.com>
2021-07-21 15:46:09 +0700

    S4 update
    - New physics engine for more realistic gameplay
    - New UI for showing beys' stats

--- bey.js
+++ bey.js
@@ -10,6 +10,8 @@ class Bey {
     this.driver = driver;
 
 		this.spinVel = createVector(0, 0); // to maitain spiral movement
+    this.lastSpinVect = createVector(0, 0);
+    this.angle = 0;
 		this.cw = 1.1; // spin CCW or CW
 
 		//this.m = 100; // mass
@@ -27,8 +29,10 @@ class Bey {
 		this.stm = (top.stm + ring.stm + driver.stm) / 3; // stamina
 		this.maxStm = (top.stm + ring.stm + driver.stm) / 3;
 
-    this.rpm = 43000;
-    this.maxRpm = 43000;
+    this.rpm = 5000;
+    this.maxRpm = 5000;
+
+    this.frt = 0.001; // friction
 
     this.name = top.color[3].slice(-2) + "-" + ring.color[3].slice(-2) + "-" + driver.color[3].slice(-2);
 	}
@@ -50,7 +54,7 @@ class Bey {
   	return h;
   }
 
-  reflect(h) { // h is the point that collides
+  reflect(h, other) { // h is the point that collides
   	let centerToH = p5.Vector.sub(h, stadium.pos);
 
   	let fixPos = centerToH.copy().setMag(stadium.r - this.r);
@@ -73,32 +77,37 @@ class Bey {
     let angle = this.vel.angleBetween(HToCenter);
     let absAngle = PI / 2 - abs(angle);
 
-    this.health -= map(absAngle, 0, PI / 2, 0, 500);
+    this.frt = 0.001;
+    if (other.checkDeath() === 0) {
+      this.health -= map(absAngle, 0, PI / 2, 0, 500);
+      this.rpm -= map(absAngle, 0, PI / 2, 0, 50);
+    }
   }
 
   friction() {
 
-  	let friction = this.vel.copy();
-  	friction.normalize();
-  	friction.mult(-1);
+  	let f = this.vel.copy();
+  	f.normalize();
+  	f.mult(-1);
 
-  	friction.setMag(mu);
-  	this.applyForce(friction);
+  	f.setMag(this.frt);
+  	this.applyForce(f);
   }
 
   spin() {
   	let centerToPos = p5.Vector.sub(stadium.pos, this.pos);
     let rotation = map(centerToPos.mag(), 0, stadium.r - this.r, 1, 2);
-    this.spinVel.setHeading(centerToPos.heading() + 1.5 * PI / 2);
+    this.spinVel.setHeading(centerToPos.heading() +  1.5 * PI / 2);
   	let velHead = createVector(this.vel.x + this.spinVel.x, this.vel.y + this.spinVel.y).heading();
-  	this.vel.setHeading(velHead).limit(14);
+  	//this.vel.setHeading(velHead).limit(14);
+    this.vel.setHeading(velHead);
   }
-
+  
   checkCollision(other) {
-    // Get distances between the balls components
+    // Get distance vector
     let distVect = p5.Vector.sub(this.pos, other.pos);
 
-    // Calculate magnitude of the vector separating the balls
+    // Calculate magnitude of the vector
     let distVectMag = distVect.mag();
 
     // Minimum distance before they are touching
@@ -109,6 +118,7 @@ class Bey {
       //this.vel.setHeading(newVel.heading()).mult(1.5);
 
       this.attack(other);
+      this.frt = 0.001;
     }
   }
 
@@ -171,11 +181,37 @@ class Bey {
   }
 
   update() {
+
   	this.vel.add(this.acc);
   	this.pos.add(this.vel);
   	this.acc.set(0, 0);
 
-    this.calcStm();
+    // max speed based on rpm
+    if (this.rpm < 700) {
+      this.vel.limit(8);
+    } else {
+      this.vel.limit(14);
+    }
+
+    if (this.frt >= this.vel.mag()) {
+      this.frt = this.vel.mag();
+    }
+
+    if (this.health < 0) {
+      this.health = 0;
+    }
+
+    if (this.rpm < 0) {
+      this.rpm = 0;
+    }
+  }
+
+  updateStm(other) {
+
+    if (other.checkDeath() === 0) {
+      this.calcStm();
+      this.frt += 0.0003;
+    }
   }
 
   show() {
@@ -196,35 +232,82 @@ class Bey {
     fill(this.driver.color[0], this.driver.color[1], this.driver.color[2]); // driver
   	circle(this.pos.x, this.pos.y, this.r * 2 / 3);
   	pop();
+  }
 
-  	push();
-  	if (this.statPos === 0) {
+  showStat() {
+    push();
+    if (this.statPos === 0) {
       textSize(16);
-      text(this.name, 10, 520); // name
-  		stroke(0);
-  		strokeWeight(1);
-  		noFill();
-  		rect(0, 540, 200, 10); // max health
-  		rect(0, 560, 200, 10); // max stm
-  		noStroke();
-  		fill(0, 255, 0);
-  		rect(0, 540, map(this.health, 0, this.maxHealth, 0, 200, true), 10); // current health
-  		fill(243, 239, 45);
-  		rect(0, 560, map(this.rpm, 0, this.maxRpm, 0, 200, true), 10); // current stm
-  	} else {
+      text("atk: " + this.atk, 10, 480); // atk
+      text("def: " + this.def, 10, 500); // def
+      text("sta: " + round(this.stm * 10) / 10, 10, 520); // sta
+      stroke(0);
+      strokeWeight(1);
+      noFill();
+      rect(0, 530, 240, 20); // max health
+      rect(0, 550, 240, 20); // max rpm
+      noStroke();
+      fill(0, 255, 0);
+      rect(0, 530, map(this.health, 0, this.maxHealth, 0, 240, true), 20); // current health
+      textSize(14);
+      fill(0);
+      textAlign(CENTER);
+      text(round(this.health), 120, 546);
+      fill(243, 239, 45);
+      rect(0, 550, map(this.rpm, 0, this.maxRpm, 0, 240, true), 20); // current rpm
+      fill(0);
+      textAlign(CENTER);
+      text(round(this.rpm), 120, 566);
+      textSize(20);
+      textStyle(BOLD);
+      textAlign(CENTER);
+      text(this.name, 120, 600); // name
+      rectMode(CENTER);
+      fill(this.top.color[0], this.top.color[1], this.top.color[2]);
+      stroke(0);
+      strokeWeight(1);
+      rect(200, 481.5, 50, 12);
+      fill(this.ring.color[0], this.ring.color[1], this.ring.color[2]);
+      rect(200, 492.5, 35, 10);
+      fill(this.driver.color[0], this.driver.color[1], this.driver.color[2]);
+      rect(200, 502.5, 10, 10);
+
+    } else {
       textSize(16);
-      text(this.name, 310, 520); // name
-  		stroke(0);
-  		strokeWeight(1);
-  		noFill();
-  		rect(300, 540, 200, 10); // max health
-  		rect(300, 560, 200, 10); // max stm
-  		noStroke();
-  		fill(0, 255, 0);
-  		rect(300, 540, map(this.health, 0, this.maxHealth, 0, 200, true), 10); // current health
-  		fill(243, 239, 45);
-  		rect(300, 560, map(this.rpm, 0, this.maxRpm, 0, 200, true), 10); // current stm
-  	}
-  	pop();
+      text("atk: " + this.atk, 420, 480); // atk
+      text("def: " + this.def, 420, 500); // def
+      text("sta: " + round(this.stm * 10) / 10, 420, 520); // sta
+      stroke(0);
+      strokeWeight(1);
+      noFill();
+      rect(260, 530, 240, 20); // max health
+      rect(260, 550, 240, 20); // max rpm
+      noStroke();
+      fill(0, 255, 0);
+      rect(width - map(this.health, 0, this.maxHealth, 0, 240, true), 530, map(this.health, 0, this.maxHealth, 0, 240, true), 20); // current health
+      textSize(14);
+      fill(0);
+      textAlign(CENTER);
+      text(round(this.health), 380, 546);
+      fill(243, 239, 45);
+      rect(width - map(this.rpm, 0, this.maxRpm, 0, 240, true), 550, map(this.rpm, 0, this.maxRpm, 0, 240, true), 20); // current rpm
+      fill(0);
+      textAlign(CENTER);
+      text(round(this.rpm), 380, 566);
+      textSize(20);
+      textStyle(BOLD);
+      textAlign(CENTER);
+      text(this.name, width - 120, 600); // name
+      rectMode(CENTER);
+      fill(this.top.color[0], this.top.color[1], this.top.color[2]);
+      stroke(0);
+      strokeWeight(1);
+      rect(width - 200, 481.5, 50, 12);
+      fill(this.ring.color[0], this.ring.color[1], this.ring.color[2]);
+      rect(width - 200, 492.5, 35, 10);
+      fill(this.driver.color[0], this.driver.color[1], this.driver.color[2]);
+      rect(width - 200, 502.5, 10, 10);
+    }
+    pop();
   }
 }
